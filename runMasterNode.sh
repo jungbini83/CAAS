801498888bc1cf006a0d823b64cbee3f27168403
jungbini <jungbini@gmail.com>
2017-01-13 16:48:16 +0900

    add function for making master's account and running master node alone

---
+++ runMasterNode.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# runMasterNode.sh <root dir>
+# <root dir>: root directory where the log, chain, address, enode are saved
+root=$1
+
+# 1. make master's account
+accountAddr=$(geth --datadir ~/$root/data/master --password <(echo -n master) account new)
+echo "Master's account address: "${accountAddr:10:40}
+echo ${accountAddr:10:40} >> ~/$root/data/master/account 	# save account address
+
+# 2. make genesis.json file including master's account
+./genGenesis.sh	$root
+
+# 3. running master node demon
+geth --datadir ~/$root/data/master init ~/$root/genesis.json
+nohup geth --datadir ~/$root/data/master --rpc --rpcport "8540" --rpcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3" \
+--port 3030 --unlock=${accountAddr:10:40} --password=<(echo -n master) --nodiscover --mine &
+
+# 4. save master node's enode address
+geth --exec 'admin.nodeInfo.enode' attach http://localhost:8540 > ~/root/data/master/enode
