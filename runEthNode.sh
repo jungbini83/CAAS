367732da0cd39d234aee9149f122b60f1bea3f17
Jung-Been Lee <jungbini@gmail.com>
2017-01-18 10:33:44 +0900

    1. auto root detection added 2. save log file to masters data directory

--- runEthNode.sh
+++ runEthNode.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-# runEthNode.sh <root dir> <node id> <connecting peer node id>
+# runEthNode.sh <root dir> <node id>
 # <root dir>: root directory where the log, chain, address, enode are saved
 # <node id>: node id written by 2 digit number format like 01, 02, 03...
-# <connecting peer node id>: node id the node want to connect as peer
-root=$1
-shift
-nn=$1
-#shift
-#cn=$1
+if [ "$#" -ne 1 ]; then
+
+	if [ -f ./"root.cfg" ]; then
+		root=$(cat ./root.cfg)
+		nn=$1
+	else
+		echo 'root.cfg file is not exist. Then you can assigne root directory manually.'
+		echo 'USAGE: runEthNode.sh <rootDir> <node id>'
+		exit 1
+	fi
+
+elif [ "$#" -ne 2 ]; then
+	root=$1
+	shift
+	nn=$1
+else
+	echo 'USAGE: runEthNode.sh <rootDir> <node id>'
+	exit 1
+fi
 
 # 1. make ethereum node account
 dd=$(printf "%02d" $nn)		# change node number to node ID like 01, 02, 03...
@@ -16,27 +29,10 @@ accountAddr=$(geth --datadir ~/$root/data/$dd --password <(echo -n $dd) account 
 echo "Node#"$dd "'s account address: "${accountAddr:10:40}
 echo ${accountAddr:10:40} >> ~/$root/data/$dd/account 	# save account address
 
-# make static-nodes.json to connect this node to master's node
-if [$cn -ne 'master']; then 		# if the cn variable is digit not 'master', 
-	cd=$(printf "%02d" $cn)			# convert 2 digit format
-else
-	cd=$cn
-fi
-
-masterEnode=$(cat ~/${root}/data/$cd/enode)
-fileContents='[
-	'
-fileContents=$fileContents$masterEnode
-fileContents=$fileContents'
-]'
-
-# connect this node to another node for connecting peer node at start
-# echo $fileContents >> ~/$root/data/$dd/static-nodes.json
-
-# 3. running ethereum node demon
+# 2. running ethereum node demon
 geth --datadir ~/$root/data/$dd init ~/$root/genesis.json
 nohup geth --datadir ~/$root/data/$dd --rpc --rpcport "85${dd}" --rpcapi "admin,db,eth,debug,miner,net,shh,txpool,personal,web3" \
---port "30${dd}" --unlock=${accountAddr:10:40} --password=<(echo -n $dd) --nodiscover &
+--port "30${dd}" --unlock=${accountAddr:10:40} --password=<(echo -n $dd) --nodiscover > ~/$root/data/$dd/log 2>&1 &
 
-# 4. save this node's enode address
+# 3. save this node's enode address
 geth --exec 'admin.nodeInfo.enode' attach http://localhost:85$dd > ~/$root/data/$dd/enode
