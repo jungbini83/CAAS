fe4d2ff30134816efb69f7f6c131260d9f495ec2
Jung-Been Lee <jungbini@gmail.com>
2017-01-19 18:04:27 +0900

    generate genesis.json file codes is added from genGenesis.sh

--- ethereum/runMasterNode.sh
+++ ethereum/runMasterNode.sh
@@ -2,7 +2,13 @@
 
 # runMasterNode.sh <root dir>
 # <root dir>: root directory where the log, chain, address, enode are saved
-root=$1
+if [ "$#" -eq 1 ]; then
+	root=$1
+else
+	"USAGE: runMasterNode.sh <rootDir>"
+	exit 1
+fi
+
 echo $root > root.cfg			# save root directory information
 
 # 0. clean existing ethereum data files
@@ -18,7 +24,26 @@ echo "Master's account address: "${accountAddr:10:40}
 echo ${accountAddr:10:40} >> ~/$root/data/master/account 	# save account address
 
 # 2. make genesis.json file including master's account
-./genGenesis.sh	$root
+genesisContent='{
+	"alloc": {
+		"'
+genesisContent=$genesisContent${accountAddr:10:40}
+genesisContent=$genesisContent'": {
+			"balance": "5000000000000000000000000000"
+		}
+	},
+
+	"nonce": "0x0000000000000000",
+	"difficulty": "0x010000",
+	"mixhash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+	"coinbase": "0x0000000000000000000000000000000000000000",
+	"timestamp": "0x00",
+	"parentHash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+	"extraData": "0x",
+	"gasLimit": "0x1000000000"
+}'
+
+echo "$genesisContent" >> ~/${root}/genesis.json
 
 # 3. running master node demon
 geth --datadir ~/$root/data/master init ~/$root/genesis.json
