cb5c7a065532a111ab0956daaae16282e398ea4e
Jung-Been Lee <jungbini@gmail.com>
2017-01-29 16:32:02 +0900

    modify generating grid map route

--- sumo/createNetRoute.sh
+++ sumo/createNetRoute.sh
@@ -12,13 +12,13 @@ fi
 
 if [ $netType == "grid" ]; then
 	netcmd="netgenerate --grid --grid.number=5 --grid.length=100 --output-file=./map/${netType}/${netType}.net.xml"
-	routecmd="python /usr/local/share/sumo/tools/randomTrips.py -n ./map/${netType}/${netType}.net.xml -e 180 -r ./map/${netType}/${netType}.rou.xml --trip-attributes=departLane=\"best\" departSpeed=\"random\" departPos=\"random\" --intermediate 10 -p 5 -b 0.1 -o ./map/${netType}/${netType}.trips.xml"		
+	routecmd="python randomTrips.py -n ./map/${netType}/${netType}.net.xml -e 180 -r ./map/${netType}/${netType}.rou.xml --trip-attributes=departLane=\"best\" departSpeed=\"random\" departPos=\"random\" --intermediate 10 -p 4 -b 0.1 -o ./map/${netType}/${netType}.trips.xml"		
 elif [ $netType == "spider" ]; then
 	netcmd="netgenerate --spider --arms=5 --circles=3 --radius=100 --output-file=./map/${netType}/${netType}.net.xml"
-	routecmd="python /usr/local/share/sumo/tools/randomTrips.py -n ./map/${netType}/${netType}.net.xml -e 180 -r ./map/${netType}/${netType}.rou.xml --trip-attributes=departLane=\"best\" departSpeed=\"random\" departPos=\"random\" --intermediate 10 -p 1 -o ./map/${netType}/${netType}.trips.xml"		
+	routecmd="python randomTrips.py -n ./map/${netType}/${netType}.net.xml -e 180 -r ./map/${netType}/${netType}.rou.xml --trip-attributes=departLane=\"best\" departSpeed=\"random\" departPos=\"random\" --intermediate 10 -p 1 -o ./map/${netType}/${netType}.trips.xml"		
 elif [ $netType == "random" ]; then
 	netcmd="netgenerate --rand --rand.iterations=30 --output-file=./map/${netType}/${netType}.net.xml"
-	routecmd="python /usr/local/share/sumo/tools/randomTrips.py -n ./map/${netType}/${netType}.net.xml -e 180 -r ./map/${netType}/${netType}.rou.xml --trip-attributes=departLane=\"best\" departSpeed=\"random\" departPos=\"random\" --intermediate 10 -p 1 -o ./map/${netType}/${netType}.trips.xml"		
+	routecmd="python randomTrips.py -n ./map/${netType}/${netType}.net.xml -e 180 -r ./map/${netType}/${netType}.rou.xml --trip-attributes=departLane=\"best\" departSpeed=\"random\" departPos=\"random\" --intermediate 10 -p 1 -o ./map/${netType}/${netType}.trips.xml"		
 
 fi
 
